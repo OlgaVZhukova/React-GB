60a2176d4263e03a10fbd44f4efc226b35d3184e
Olga Zhukova <olga.v.zhukoff@gmail.com>
2024-03-17 16:42:35 +0300

    All pages are added as components. Routing is configured. Functional of sorting and counting of actual price in the cart is realized. All Tasks for HW8 are solved

--- homework8-shop/src/App.js
+++ homework8-shop/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import CartPage from "./pages/CartPage";
+import ProductPage from "./pages/ProductPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+      </Routes>
+    </Router>
   );
 }
 
